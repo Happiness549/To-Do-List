95d89bc4991f6fc738d52f70a0692c64365ee22e
Happiness549 <mhlongohappiness880@gmail.com>
2025-03-19 19:07:13 +0200

    Updated js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const todoList = [];
+const todoList = ['make dinner','Wash dishes'];
+
+const todoListHTML = ""
+for(let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`
+    todoListHTML += html;
+
+}
+console.log(todoListHTML);
 
 function addTodo() {
 inputElement = document.querySelector("#js-name-input");
 const name = inputElement.value;
-
-todoList.push(name);
+inputElement.value = '';
 };
+
